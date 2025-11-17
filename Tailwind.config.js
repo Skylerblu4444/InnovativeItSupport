c184b413b64173a7cb0d6d8a50852cff3d6e2006
Skyler Blue Spillers <skylerblu4444@gmail.com>
2025-11-17 01:58:58 -0600

    Add Tailwind CSS configuration with custom colors

---
+++ Tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./src/**/*.{ts,tsx,js,jsx,mdx}'],
+  theme: {
+    extend: {
+      colors: {
+        horizon: {
+          50: '#f2fbff',
+          100: '#e6f7ff',
+          500: '#0ea5b1',
+          700: '#0b7285'
+        }
+      }
+    }
+  },
+  plugins: []
+};
